f75b82b73d873b7fe18110b47ffe414cc5edeb79
wukuic <53172255+wukuic@users.noreply.github.com>
2019-10-06 13:33:43 +0800

    上传demo
    demo1

---
+++ democ/src/main/java/com/example/democ/DemocApplication.java
@@ -0,0 +1,13 @@
+package com.example.democ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemocApplication.class, args);
+    }
+
+}
